3648b0cb8bae83433d444bf9b51124638e974ff5
system_username <thanhnhanmai@localhost>
2022-02-09 16:49:03 -0500

    Add ts run local url

---
+++ Test Suites/TS_Local_URL.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Local_URL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7f2267cc-5ad8-46ed-9208-a2635d3a5a1f</testSuiteGuid>
+   <testCaseLink>
+      <guid>027507a9-b7b5-4081-8fc2-5eb8fa7ab0ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Run_Local_URL</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
